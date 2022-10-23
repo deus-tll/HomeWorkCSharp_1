ba99ab3f7fa1443397ab67e077faf3ad21ee3440
deus-tll <millerartemka14@gmail.com>
2022-10-23 22:11:05 +0300

    update

---
+++ HomeWork_11/ExtensionMethod.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_11
+{
+
+	internal static class ExtensionMethod
+	{
+		/////////////
+		private static bool IsSquare(double number)
+		{
+			double tmp = Math.Sqrt(number);
+
+			return tmp - Math.Floor(tmp) == 0;
+		}
+		public static bool IsFibonacci(this int number)
+		{
+			double tmp1 = 5 * Math.Pow(number, 2) + 4;
+			double tmp2 = 5 * Math.Pow(number, 2) - 4;
+
+			return (IsSquare(tmp1) || IsSquare(tmp2)) || (IsSquare(tmp1) && IsSquare(tmp2));
+		}
+		/////////////
+
+
+		/////////////
+		public static int WordsCount(this string str)
+		{
+			string[] words = str.Split(' ');
+
+			return words.Length;
+		}
+		/////////////
+
+
+		/////////////
+		public static int LengthOfLastWord(this string str)
+		{
+			string[] words = str.Split(' ');
+
+			return words[words.Length - 1].Length;
+		}
+		/////////////
+
+
+		/////////////
+		public static bool IsValidParenthesis(this string str)
+		{
+			Stack<char> stack = new();
+
+			foreach (var c in str)
+			{
+				switch (c)
+				{
+					case '(':
+					case '[':
+					case '{':
+						stack.Push(c);
+						break;
+
+					case ')':
+						if (stack.Count == 0) return false;
+						if (stack.Pop() != '(') return false;
+						break;
+					case ']':
+						if(stack.Count == 0) return false;
+						if(stack.Pop() != '[') return false;
+						break;
+					case '}':
+						if (stack.Count == 0) return false;
+						if(stack.Pop() != '{') return false;
+						break;
+				}
+			}
+
+			return stack.Count == 0;
+		}
+		/////////////
+
+
+		/////////////
+		public static int[] Filtration(this int[] arr, Predicate<int> predicate)
+		{
+			int[] newArr = Array.FindAll<int>(arr, predicate);
+
+			return newArr;
+		}
+		/////////////
+	}
+}
---
+++ HomeWork_11/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HomeWork_11
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			///1
+			int n = 6765;
+			Console.WriteLine(n.IsFibonacci());
+			///
+
+
+			///2
+			string str = "one two three";
+			Console.WriteLine(str.WordsCount());
+			///
+
+
+			///3
+			Console.WriteLine(str.LengthOfLastWord());
+			///
+
+
+			///4
+			string str1 = "({ x - y - z} * [x + 2y] - (z + 4x));";
+			Console.WriteLine(str1.IsValidParenthesis());
+			///
+
+
+			///5
+			int[] arr = new int[] { 20, 3, 7, 16, 4, 8, 89, 31 };
+			arr = arr.Filtration(s => s % 2 == 0);
+
+			foreach (int item in arr)
+			{
+				Console.Write($"{item} ");
+			}
+			///
+		}
+	}
+}
