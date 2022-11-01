7b4644d98c00903358ec52b00f118e84532c5a41
deus-tll <millerartemka14@gmail.com>
2022-11-02 01:39:34 +0200

    update

---
+++ HomeWork_16/Currency.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HomeWork_16
+{
+	class Currency
+	{
+		public int r030 { get; set; }
+		public string? txt { get; set; }
+		public decimal rate { get; set; }
+		public string? cc { get; set; }
+		public DateTime exchangedate { get; set; }
+
+		public override string ToString()
+		{
+			return $"r030:".PadRight(24) + $"{r030}\n" +
+				   $"txt:".PadRight(24) + $"{txt}\n" +
+				   $"rate:".PadRight(24) + $"{rate}\n" +
+				   $"cc:".PadRight(24) + $"{cc}\n" +
+				   $"exchangedate:".PadRight(24) + $"{exchangedate.ToShortDateString()}\n";
+		}
+
+		private static void Show(IEnumerable values)
+		{
+			foreach (var item in values)
+			{
+				Console.WriteLine(item);
+			}
+		}
+
+		public static void ShowMoreThan20(List<Currency> l)
+		{
+			var v = from c in l
+					where c.rate > 20
+					select c;
+
+			Show(v);
+		}
+	}
+}
---
+++ HomeWork_16/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Globalization;
+using System.Runtime.ConstrainedExecution;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace HomeWork_16
+{
+	internal class Program
+	{
+		static void Parsing(List<Currency> list, XmlDocument xml)
+		{
+			if (xml.DocumentElement is not null)
+			{
+				XmlElement? xmlRoot = (XmlElement?)xml.DocumentElement;
+
+				if (xmlRoot is not null)
+				{
+					foreach (XmlNode rNode in xmlRoot)
+					{
+						Currency cur = new Currency();
+
+						foreach (XmlNode childNode in rNode)
+						{
+							if (childNode.Name == "r030")
+							{
+								cur.r030 = Convert.ToInt32(childNode.InnerText);
+							}
+							else if (childNode.Name == "txt")
+							{
+								cur.txt = childNode.InnerText;
+							}
+							else if (childNode.Name == "rate")
+							{
+								NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+								{
+									NumberDecimalSeparator = ".",
+								};
+
+								cur.rate = Decimal.Parse(childNode.InnerText, numberFormatInfo);
+							}
+							else if (childNode.Name == "cc")
+							{
+								cur.cc = childNode.InnerText;
+							}
+							else if (childNode.Name == "exchangedate")
+							{
+								DateTimeFormatInfo dateTimeFormatInfo = new DateTimeFormatInfo()
+								{
+									ShortDatePattern = "d.M.yyyy",
+									DateSeparator = "."
+								};
+
+								cur.exchangedate = DateTime.Parse(childNode.InnerText);
+							}
+						}
+
+						list.Add(cur);
+					}
+				}
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.Unicode;
+			Console.InputEncoding = System.Text.Encoding.Unicode;
+
+
+			List<Currency> list = new List<Currency>();
+			XmlDocument xml = new XmlDocument();
+			xml.Load("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
+
+			Parsing(list, xml);
+			
+			Currency.ShowMoreThan20(list);
+		}
+	}
+}
